b135b8e2ee72acb751400a2605349e2bd5bfaa97
Eduardo Angulo <edangulo@uninorte.edu.co>
2023-09-18 20:13:11 -0500

    Modified book module and main file

---
+++ src/book.py
@@ -0,0 +1,41 @@
+from abc import ABC
+from typing import Any, List
+
+class Libro(ABC):
+
+    def __init__(self, titulo: str, isbn: str, genero: str, formato: str, valor: float, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self._titulo = titulo
+        self._autores: List["Autor"] = []
+        self._isbn = isbn
+        self._genero = genero
+        self._formato = formato
+        self._valor = valor
+        self._editorial: "Editorial" = None
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self._titulo!r}, {self._isbn!r})'
+
+
+class LibroImpreso(Libro):
+
+    def __init__(self, paginas: int, num_ejemplares: int, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.__paginas = paginas
+        self.__num_ejemplares = num_ejemplares
+
+
+class LibroDigital(Libro):
+
+    def __init__(self, has_hipervinculo: bool, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.__has_hipervinculo = has_hipervinculo
+        self.__hipervinculos: List[str] = []
+
+
+class Audiolibro(Libro):
+
+    def __init__(self, duracion: int, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.__duracion = duracion
+        self.__narrador: "Narrador" = None
--- src/main.py
+++ src/main.py
@@ -1,12 +1,45 @@
+from book import LibroImpreso, LibroDigital, Audiolibro
 from person import Autor
 
 def main() -> None:
-    nombre = input('Digite el nombre del autor: ')
-    cedula = int(input('Digite el cedula del autor: '))
+    #nombre = input('Digite el nombre del autor: ')
+    #cedula = int(input('Digite el cedula del autor: '))
     
-    autor = Autor(nombre, cedula)
+    #autor = Autor(nombre, cedula)
 
-    print(autor)
+    #print(autor)
+
+    libro_1 = LibroImpreso(
+        titulo = 'Cien años de soledad',
+        isbn = '848559455',
+        genero = 'Novela',
+        formato = 'Tapa dura',
+        valor = 85000.0,
+        paginas = 496,
+        num_ejemplares = 5
+    )
+
+    libro_2 = LibroDigital(
+        titulo = 'El aliento de los dioses',
+        isbn = '5848485984',
+        genero = 'Novela',
+        formato = 'pdf',
+        valor = 60000.0,
+        has_hipervinculo = True
+    )
+
+    libro_3 = Audiolibro(
+        titulo = 'Harry Potter',
+        isbn = '1896455814',
+        genero = 'Novela',
+        formato = 'mp4',
+        valor = 50000.0,
+        duracion = 330
+    )
+
+    print(libro_1)
+    print(libro_2)
+    print(libro_3)
 
 if __name__ == '__main__':
     main()
